fea5526259a966051c6d7e874bdca56a97b7b8e3
Patricio Bruno Laptop <pdbruno@gmail.com>
2022-10-15 17:54:30 -0300

    corridas y cimbala

--- cimbala.py
+++ cimbala.py
@@ -7,7 +7,7 @@ def outliers(data):
   S = st.stdev(data)
   alpha = 0.05
   n = len(data)
-  t_alfa2 = t.ppf(alpha, n-2)
+  t_alfa2 = t.ppf(1-alpha/2, n-2)
   tau = (t_alfa2 * (n-1))/(np.sqrt(n) * np.sqrt(n - 2 + t_alfa2**2))
 
   return [muestra for muestra in data if np.abs(muestra - prom) > tau * S]

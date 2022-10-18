2a89407060f3f3c6dfe9a1b79c79fc55fa026d7d
Patricio Bruno Laptop <pdbruno@gmail.com>
2022-10-18 00:07:59 -0300

    genero los graficos para las 3 universidades

--- plot_world_map.py
+++ plot_world_map.py
@@ -23,18 +23,20 @@ def request_ips(ips):
     return pd.json_normalize(response.json())
 
 
-with open('traceroute_results/uonbi_ac_ke_routes.json') as f:
-    routes = json.load(f)
+for name in ['bsu_by_routes', 'uonbi_ac_ke_routes', 'vnu_edu_vn_routes']:
 
-route_number = 1
-df = pd.DataFrame(columns=['lat', 'lon'])
-for ips in routes:
-    result = request_ips(ips)
-    result['route'] = 'route ' + str(route_number)
-    df = pd.concat([df, result])
-    route_number += 1
-print(df.to_string())
-fig = px.line_geo(df, lat="lat", lon="lon",
-                  projection="equirectangular", color='route')
-fig.write_html("images/uonbi_ac_ke_routes.html")
-fig.write_image("images/uonbi_ac_ke_routes.png")
+    with open(f'traceroute_results/{name}.json') as f:
+        routes = json.load(f)
+
+    route_number = 1
+    df = pd.DataFrame(columns=['lat', 'lon'])
+    for ips in routes:
+        result = request_ips(ips)
+        result['route'] = 'route ' + str(route_number)
+        df = pd.concat([df, result])
+        route_number += 1
+    print(df.to_string())
+    fig = px.line_geo(df, lat="lat", lon="lon",
+                      projection="equirectangular", color='route')
+    fig.write_html(f"images/{name}.html")
+    fig.write_image(f"images/{name}.png")
